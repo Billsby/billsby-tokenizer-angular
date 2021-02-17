94025086ffce201a8b381f0237c456787a314f75
Francis <francisfernandez@fullstack.ph>
2021-02-17 18:40:40 +0800

    Finished payment functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardTokenizerComponent } from './card-tokenizer/card-tokenizer.component';
 import { HeaderComponent } from './header/header.component';
+import { PrettyPrintPipe } from './pretty-print.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardTokenizerComponent,
-    HeaderComponent
+    HeaderComponent,
+    PrettyPrintPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card-tokenizer/card-tokenizer.component.ts
+++ src/app/card-tokenizer/card-tokenizer.component.ts
@@ -6,11 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-tokenizer.component.css']
 })
 export class CardTokenizerComponent implements OnInit {
-  public name = "Francis";
-  
-  constructor() { }
+ 
+  // Declare this so you can use the billsby payment libraries/methods/hooks
+  public billsbyTokens = (<any>window).billsbyTokens;
+  payment_info= [];
+  payment_info2= [];
+  boboka=[];
+  constructor() { 
+    
+    if(this.billsbyTokens) {
+      this.billsbyTokens.init("billsby-number", "billsby-cvv");
+      this.billsbyTokens.on("ready", function () {
+        const submitButton = document.getElementById("submit-button");
+        
+      });
+        
+      this.billsbyTokens.on("errors", function (errors: any) {
+          for (var i = 0; i < errors.length; i++) {
+            var error = errors[i];
+            console.log(error);
+          }
+        });
 
+      this.billsbyTokens.on("paymentMethod",  ( token: any, pmData: any) => {
+          this.payment_info2 = pmData;
+              console.log(token);
+              console.log(pmData);
+        });
+    }
+  }
+
+  public paymentForm = {
+    full_name: null,
+    month: null,
+    year: null,
+  };
+
+  onSubmit(): void{
+    this.paymentForm = {
+      full_name: this.paymentForm.full_name,
+      month: this.paymentForm.month,
+      year:  this.paymentForm.year
+    };
+    this.billsbyTokens.tokenizeCreditCard(this.paymentForm);
+    console.log(this.payment_info2);
+  }
+  
   ngOnInit(): void {
+    
   }
 
 }
