2ab9a2c1d04af95709e1f09fc5121ae99f5a5983
Francis <francisfernandez@fullstack.ph>
2021-02-18 18:39:05 +0800

    finish header bar responsiveness

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  toggleMenu = false;
+  toggleMenu = true;
   constructor(){ }
 
   handleToggleMenu(){
