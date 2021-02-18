07a7af910ff282d6eeeda99c35c995b2fa7a3585
Francis <francisfernandez@fullstack.ph>
2021-02-18 15:12:39 +0800

    initial commit

--- src/app/card-tokenizer/card-tokenizer.component.ts
+++ src/app/card-tokenizer/card-tokenizer.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: '[app-card-tokenizer]',
   templateUrl: './card-tokenizer.component.html',
-  styleUrls: ['./card-tokenizer.component.css']
 })
 export class CardTokenizerComponent implements OnInit {
  
   // Declare this so you can use the billsby payment libraries/methods/hooks
-  public billsbyTokens = (<any>window).billsbyTokens;
-  payment_info= [];
-  payment_info2= [];
+  billsbyTokens = (<any>window).billsbyTokens;
+  set_payment_info= '';
+  get_payment_info= '';
   boboka=[];
   constructor() { 
     
@@ -20,28 +21,28 @@ export class CardTokenizerComponent implements OnInit {
         const submitButton = document.getElementById("submit-button");
         
       });
-        
       this.billsbyTokens.on("errors", function (errors: any) {
           for (var i = 0; i < errors.length; i++) {
             var error = errors[i];
             console.log(error);
           }
         });
-
       this.billsbyTokens.on("paymentMethod",  ( token: any, pmData: any) => {
-          this.payment_info2 = pmData;
-              console.log(token);
-              console.log(pmData);
+        this.set_payment_info = pmData;
+          console.log(token);
+          console.log(pmData);
         });
+    
     }
   }
-
+  
   public paymentForm = {
     full_name: null,
     month: null,
     year: null,
   };
 
+  // Once clicked submit button
   onSubmit(): void{
     this.paymentForm = {
       full_name: this.paymentForm.full_name,
@@ -49,11 +50,21 @@ export class CardTokenizerComponent implements OnInit {
       year:  this.paymentForm.year
     };
     this.billsbyTokens.tokenizeCreditCard(this.paymentForm);
-    console.log(this.payment_info2);
+
+    // Will update data in view
+    let stop = false;
+    interval(900)
+    .pipe(takeWhile(() => !stop))
+    .subscribe(() => {
+        this.get_payment_info = JSON.stringify(this.set_payment_info, undefined, 4)
+          .replace(/ /g, '&nbsp;')
+          .replace(/\n/g, '<br/>');
+    });
   }
   
   ngOnInit(): void {
     
+   
   }
 
 }
